c13df631f9414c26dd64b32b31cf652988760c78
wangpeng16@100tal.com <wangpeng16@100tal.com>
2020-06-11 14:03:06 +0800

    opmz: 优化一波数据处理

--- src/index.js
+++ src/index.js
@@ -26,12 +26,27 @@ function crawlerFactory(crawler, uri, fileName, category_id) {
         topics.forEach((topic) => {
           if (!topic.includes('##') && topic.includes('```')) {
             const arr = topic.split('```');
-            let title = arr[0];
+            let title = arr.splice(0, 1)[0];
             const idx = title.indexOf('.');
             title = title.substring(idx + 1, title.length).trim();
+
+            const contentArr = arr.join('\r\n').split('\r\n');
+            const reg = new RegExp("[\\u4E00-\\u9FFF]+$","g");
+            const contentResult = [];
+            contentArr.forEach((ctn) => {
+              if (!!ctn) {
+                contentResult.push(ctn.trim());
+                const lastWord = ctn.trim().substring(ctn.length - 1, ctn.length);
+                const blacklist = [',', '，'];
+                if (!reg.test(lastWord) && !blacklist.includes(lastWord) && !/^[a-zA-Z0-9]$/.test(lastWord)) { // 以中文结尾
+                  contentResult.push('\n');
+                }
+              }
+            })
+
             const item = {
               title: `#### ${utils.removeLinebreak(title)}`,
-              content: utils.removeLinebreak(arr[1]),
+              content: contentResult.join(''),
               status: 0,
               label: fileName,
               category_id,
